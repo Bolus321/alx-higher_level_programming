60693706bc52721d938a906bb8baca99836e166e
Bolus321 <nnamdiogbolu321@gmail.com>
2023-03-30 15:37:23 -0700

    6-peak.py corr

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,31 +1,14 @@
 #!/usr/bin/python3
 """
-Function that finds a peak in a list of unsorted integers
+Module to find the hightest number in an array
 """
 
 
 def find_peak(list_of_integers):
     """
-    finds num that's greater than both left and right
+    Method to find hightest number in array
     """
-    if len(list_of_integers) == 0:
+    if bool(list_of_integers) is False:
         return None
-
-    lists = list_of_integers
-    beg = 0
-    end = len(lists)-1
-
-    if lists[beg] > lists[beg+1]:
-        return lists[beg]
-    if lists[end] > lists[end-1]:
-        return lists[end]
-
-    mid = (beg+end)//2
-    if lists[mid-1] < lists[mid] and lists[mid+1] < lists[mid]:
-        return lists[mid]
-    if lists[mid] < lists[mid-1]:
-        return find_peak(lists[beg:mid+1])
-    elif lists[mid] < lists[mid+1]:
-        return find_peak(lists[mid:end+1])
-    else:
-        return lists[beg]
+    list_of_integers.sort()
+    return list_of_integers[-1]
